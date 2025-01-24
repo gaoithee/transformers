6a03bacf83dec8cb4a5d1f0c9bbf15fcae7c6bc3
Sara Candussio <scanduss@login07.leonardo.local>
2025-01-24 11:51:50 +0100

    leonardo correct sh file

--- src/transformers/models/stldec/train16.sh
+++ src/transformers/models/stldec/train16.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #SBATCH --no-requeue
-#SBATCH --account=dssc
-#SBATCH --job-name="test16"
-#SBATCH --get-user-env
-#SBATCH --partition=GPU
+#SBATCH --job-name="ultratest"
+#SBATCH --account IscrC_IRA-LLMs
+#SBATCH --partition=boost_usr_prod
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gpus=1                  # <-- TODO adjust this
-#SBATCH --mem=50G                             # <-- TODO adjust this
-#SBATCH --time=04:00:00                       # <-- TODO adjust this
-#SBATCH --output=slurm_outs/correct_training_16.out
+#SBATCH --gres=gpu:1
+#SBATCH --exclusive
+#SBATCH --time=00:10:00
+#SBATCH --mem=481G
+#SBATCH --output=slurm_outputs/ultratest.out
 
+module load python/3.11.6--gcc--8.5.0
+
+echo "Running on $SLURM__NNODES nodes"
 
 # Standard preamble for debugging
 echo "---------------------------------------------"
@@ -22,26 +23,14 @@ echo "DATE:                $(date)"
 echo "---------------------------------------------"
 
 
-# Needed sourcing
-source .venv/bin/activate
+source /leonardo/home/userexternal/scanduss/.venv/bin/activate
 
-# Needed modules
-# module load <module_name>
 
 # Needed exports
 # export <export_name>=<export_value>
 #variables
-FILE_NAME=/u/dssc/scandu00/transformers/src/transformers/models/stldec/train-16batch.py
 
-CMD="python3 -u"
+srun python3 train-16batch.py
 
-if [ ! -f "$FILE_NAME" ]; then
-  echo "The file $FILE_NAME does not exist"
-  exit 1
-fi
-
-# Other checks there
-
-$CMD $FILE_NAME
 
 echo "DONE!"
